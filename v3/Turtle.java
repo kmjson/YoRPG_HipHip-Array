2e175230379018c8ed12fe5720fb53f9b4e95cbc
Derek Song <dereksong@Haes-Air.home>
2017-11-13 23:09:41 -0500

    New Class Turtle

---
+++ v3/Turtle.java
@@ -0,0 +1,18 @@
+// Team: Hip Hip, Array! - Jason Kim, Derek Song, Muhammad Kashf Mashrafi
+// APCS1 pd1
+// HW30 - Ye Olde Role Playing Game, Expanded
+// 2017 - 11 - 13
+
+public class Turtle {
+    public Turtle () {
+	HP = 150;
+	strength = 100;
+	defense = 60;
+	atkRate = 0.4;
+    }
+
+    public static about() {
+	return "This is a Turtle! But he is not your average turtle that you keep at home! He is a turtle that can withstand a ton of damage and still be able to hurt you back! Don't get on his bad side or else he'll make sure he makes your attacks worthless.";
+    }
+}
+}
